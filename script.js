988c2a959a6ed86fc6a768c6d2d3d868a4f8e264
Jose Shardin <joseruiz.sh@gmail.com>
2024-04-23 10:27:08 -0500

    Agregamos el Javascript y está la aplicación de clima ista

---
+++ script.js
@@ -0,0 +1,53 @@
+let urlBase = 'https://api.openweathermap.org/data/2.5/weather'
+let api_key = '011b0f1e156ddcbf141c2ff476fadeb0'
+let difKelvin = 273.15
+
+document.getElementById('botonBusqueda').addEventListener('click', () => {
+  const ciudad = document.getElementById('ciudadEntrada').value;
+  if(ciudad) {
+    fetchDatosClima(ciudad)
+  }
+})
+
+function fetchDatosClima(ciudad) {
+
+  fetch(`${urlBase}?q=${ciudad}&appid=${api_key}`)
+    .then(data => data.json())
+    .then(data => mostrarDatosClima(data))
+
+}
+
+function mostrarDatosClima(data){
+
+  const divDatosClima = document.getElementById('datosClima')
+  divDatosClima.innerHTML = ''
+
+  const ciudadNombre = data.name
+  const paisNombre = data.sys.country
+  const humedad = data.main.humidity
+  const temperatura = data.main.temp
+  const descripcion = data.weather[0].description
+  const icono = data.weather[0].icon
+
+  const ciudadTitulo = document.createElement('h2')
+  ciudadTitulo.textContent = `${ciudadNombre}, ${paisNombre}`
+
+  const temperaturaInfo = document.createElement('p')
+  temperaturaInfo.textContent = `La temperatura es: ${Math.floor(temperatura-difKelvin)}*C`
+  
+  const humedadInfo = document.createElement('p')
+  humedadInfo.textContent = `La humedad es: ${humedad} %`
+
+  const descripcionInfo = document.createElement('p')
+  descripcionInfo.textContent = `La descripción es: ${descripcion}`
+
+  const iconoInfo = document.createElement('img')
+  iconoInfo.src = `https://openweathermap.org/img/wn/${icono}@2x.png`
+
+  divDatosClima.appendChild(ciudadTitulo)
+  divDatosClima.appendChild(temperaturaInfo)
+  divDatosClima.appendChild(humedadInfo)
+  divDatosClima.appendChild(iconoInfo)
+  divDatosClima.appendChild(descripcionInfo)
+}
+
